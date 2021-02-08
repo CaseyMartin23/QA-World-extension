23c0fe004f0d3026dbd641bdec1ba4a1e62d7d2b
CaseyMartin23 <cm232311@gmail.com>
2021-02-08 14:40:42 +0200

    Second commit

---
+++ background.js
@@ -0,0 +1,86 @@
+const qaWorldLink = "https://app.qa-world.com/calls";
+const actionLogStyle = "color: #34eb37";
+const infoStyle = "color: #ea4dff";
+
+const injectForegroundScript = () => {
+  console.log("%cI'm looking for QA world's calls tab...", actionLogStyle);
+
+  chrome.windows.getAll({}, (allWindows) => {
+    allWindows.map((window) => {
+      chrome.tabs.query({ windowId: window.id }, (allWindowTabs) => {
+        allWindowTabs.map((tab) => {
+          console.log("%cTab-URL: ", infoStyle, tab.url);
+          if (tab.url === qaWorldLink) {
+            console.log(
+              "%cI'm checking if the script has already been injected...",
+              actionLogStyle
+            );
+
+            chrome.tabs.executeScript(tab.id, {
+              code: `
+              try{
+                console.log("transcriptDiv check on back-end ->", transcriptDiv)
+              } catch (err) {
+                if(err.message === "transcriptDiv is not defined") {
+                  chrome.runtime.sendMessage({ injectScript: "inject the script" })
+                }
+                if(err.message === "Identifier 'transcriptDiv' has already been declared") {
+                  chrome.runtime.sendMessage({ injectScript: "skip script injection" })
+                }
+              }
+                    `,
+            });
+
+            chrome.runtime.onMessage.addListener(
+              (message, sender, sendResponse) => {
+                if (
+                  message &&
+                  message.injectScript &&
+                  message.injectScript === "inject the script"
+                ) {
+                  console.log("%cI'm injecting the script...", actionLogStyle);
+                  chrome.tabs.executeScript(
+                    tab.id,
+                    { file: "./front-end/checkForTranscripts.js" },
+                    () => console.log("%cScript injected!", actionLogStyle)
+                  );
+                }
+
+                if (
+                  message &&
+                  message.injectScript &&
+                  message.injectScript === "skip script injection"
+                ) {
+                  console.log(
+                    "%cI'm skipping script injection",
+                    actionLogStyle
+                  );
+                  chrome.runtime.onMessage.removeListener();
+                  return;
+                }
+
+                chrome.runtime.onMessage.removeListener();
+              }
+            );
+          }
+        });
+      });
+    });
+  });
+};
+
+chrome.tabs.onCreated.addListener(() => {
+  injectForegroundScript();
+  chrome.tabs.onCreated.removeListener();
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  injectForegroundScript();
+  if (tab.url === qaWorldLink && changeInfo.status === "complete") {
+    chrome.tabs.executeScript(tabId, {
+      code: `console.log("%cIM HERE !!!", "color: #0394fc");`,
+    });
+  }
+
+  chrome.tabs.onUpdated.removeListener();
+});
---
+++ front-end/checkForTranscripts.js
@@ -0,0 +1,58 @@
+try {
+  const transcriptDiv = document.getElementsByClassName("container-md")[0]
+    .children[2].children[0].children[0].children[0];
+
+  const fakeLink = document.createElement("a");
+  fakeLink.href =
+    "https://www.google.com/search?q=color+picker&oq=color+p&aqs=chrome.0.69i59j69i57j0j0i20i263j46j0l3.6341j0j7&sourceid=chrome&ie=UTF-8";
+  document.body.appendChild(fakeLink);
+
+  const getBestLink = () => {
+    const allTableRows = Array.from(transcriptDiv.getElementsByTagName("tr"));
+    let highestTime = 0;
+    let highestTimeLinkIndex = 0;
+
+    allTableRows.shift();
+    allTableRows.map((element, index) => {
+      const linkTime = parseFloat(element.children[2].innerHTML.split(" ")[0]);
+
+      console.log("%ctr: ", "color: #fc03f4", element.outerHTML);
+      console.log("linkTime->", linkTime);
+
+      if (highestTime < linkTime) {
+        highestTime = linkTime;
+        highestTimeLinkIndex = index;
+      }
+    });
+
+    const bestLink = allTableRows[highestTimeLinkIndex].children[0].children[0];
+
+    console.log("%cHighestLinkTime: ", "color: #ffdd00", highestTime);
+    console.log(
+      "%cNumber of Links:",
+      "color: #00ffdd",
+      Array.from(transcriptDiv.querySelectorAll("a[id^='queue-']")).length
+    );
+
+    fakeLink.click();
+    console.log("%chighestTimeLink->", "color: #ffdd00", bestLink.outerHTML);
+  };
+
+  const isTranscriptsAvailable = () => {
+    const noTranscrpits =
+      '<div class="card-body"><h1 class="large">Transcripts Available to Edit</h1><p>There are no calls at this time, please check back later.</p></div>';
+
+    console.log("%cChecking for Transcriptions...", "color: #34eb37");
+
+    if (noTranscrpits === transcriptDiv.outerHTML) {
+    } else {
+      console.log("transcriptDiv->", transcriptDiv.outerHTML);
+      getBestLink();
+      return true;
+    }
+  };
+
+  setInterval(isTranscriptsAvailable, 2000);
+} catch (err) {
+  console.error(err.message);
+}
